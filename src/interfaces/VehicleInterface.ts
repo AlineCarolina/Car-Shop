122eaa9da012ddff277686c0ffb1b143ee54e97e
Aline <eualine2017@gmail.com>
2022-04-28 17:05:09 -0300

    Requisito 2

---
+++ src/interfaces/VehicleInterface.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const VehicleInterface = z.object({
+  model: z.string()
+    .min(3, { message: 'Model must be at least 3 characters' }),
+  year: z.number()
+    .min(1900, { message: 'Year must be at least 1900' })
+    .max(2020, { message: 'Year must be at most 2020' }),
+  color: z.string()
+    .min(3, { message: 'Color must be at least 3 characters' }),
+  status: z.boolean().optional(),
+  buyValue: z.number()
+    .int({ message: 'Buy value must be an integer' }),
+});
+
+export type Vehicle = z.infer<typeof VehicleInterface>;

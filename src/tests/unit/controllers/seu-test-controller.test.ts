9915715218612bf1d6cc37eb88839657a68a97bd
Aline <eualine2017@gmail.com>
2022-04-30 15:50:43 -0300

    Requisito 7

--- src/tests/unit/controllers/seu-test-controller.test.ts
+++ src/tests/unit/controllers/seu-test-controller.test.ts
@@ -1,27 +1,63 @@
-// template para criação dos testes de cobertura da camada de controller
+import chai from 'chai';
+import chaiHttp = require('chai-http');
+import Sinon = require('sinon');
+import App from '../../../app';
+import CarController from '../../../controllers/CarController';
+import { Car } from '../../../interfaces/CarInterface';
+import Router from '../../../routes';
+import CarService from '../../../services/CarService';
+import { CarMockInterface, carMockReq } from '../../mocks/CarMock';
 
+chai.use(chaiHttp);
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// import chaiHttp = require('chai-http');
+const { expect } = chai;
 
+describe('CarController', () => {
+  let carService = new CarService();
 
-// chai.use(chaiHttp);
+  let carController = new CarController(carService);
 
-// const { expect } = chai;
+  let carRouter = new Router<Car>(carController);
 
-// describe('Sua descrição', () => {
+  carRouter.init();
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  let server = new App()
+  
+  server.addRouter(carRouter.router);
 
-//   after(()=>{
-//     ().restore();
-//   })
+  describe('#create', async () => {
+    it('should create a new car', async () => {
+      Sinon.stub(carService, 'create').resolves(CarMockInterface);
+      const res = await chai.request(server.app)
+        .post('/cars')
+        .send({
+          ...carMockReq,
+        });
+      expect(res.status).to.be.equal(201);
+      expect(res.body).to.be.an('object');
+      expect(res.body).to.have.property('_id');
+      expect(res.body).to.have.property('model');
+      expect(res.body).to.have.property('year');
+      expect(res.body).to.have.property('color');
+      expect(res.body).to.have.property('buyValue');
+      expect(res.body).to.have.property('seatsQty');
+      expect(res.body).to.have.property('doorsQty');
+      Sinon.restore();
+    });
 
-//   it('', async () => {});
-
-// });
+    it('should return a 400 error when the car is not valid', async () => {
+      Sinon.stub(carService, 'create').resolves({
+        error: 'Error',
+      });
+      const res = await chai.request(server.app)
+        .post('/cars')
+        .send({
+          ...carMockReq,
+          doorsQty: 15,
+        });
+      expect(res.status).to.be.equal(400);
+      expect(result.body).to.be.deep.eq({ error: 'Error' });
+      Sinon.restore();
+    });
+  });
+});
